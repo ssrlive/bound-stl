a799f41163a351ba70455bd31edaf8fce16dfc2f
ssrlive <30760636+ssrlive@users.noreply.github.com>
2023-02-28 13:05:51 +0800

    BTreeMap and BTreeSet added

--- readme.md
+++ readme.md
@@ -1,6 +1,14 @@
 # Bound-STL
 
-Bound-STL attempts to implement `lower_bound` and `upper_bound` in C++ STL.
+Bound-STL attempts to implement Rust copy of `lower_bound` and `upper_bound` manners in C++ STL.
+
+This implementation is adding two traits `LowerBound` and `UpperBound` to the follow structures:
+- `[..]`
+- `Vec`
+- `VecDeque`
+- `BinaryHeap`
+- `BTreeset`
+- `BTreeMap`
 
 This repo hosts at [bound-stl](https://github.com/ssrlive/bound-stl/)
 
@@ -14,14 +22,14 @@ This repo hosts at [bound-stl](https://github.com/ssrlive/bound-stl/)
 use bound_stl::LowerBound;
 
 let v = vec![1, 2, 3, 4, 5];
-assert_eq!(v.lower_bound(&3), Some(2));
+assert_eq!(v.lower_bound(&3), Ok(2));
 assert_eq!(v.lower_bound(&6), Err(5));
 
 
 use bound_stl::UpperBound;
 
 let v = vec![1, 2, 3, 4, 5];
-assert_eq!(v.upper_bound(&3), Some(3));
+assert_eq!(v.upper_bound(&3), Ok(3));
 assert_eq!(v.upper_bound(&6), Err(5));
 
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,9 @@
 use std::collections::*;
 use std::{cmp::Ordering, result::Result};
 
-/// find first index where arr[idx] >= v; assume arr is sorted
+/// find first index where arr[idx] >= v; assume arr is sorted.
+/// it is a encapsulation of `position` method, like `position(|e| e.cmp(x) != Ordering::Less)`,
+/// but it returns `Err` when all elements are less than `x`.
 pub trait LowerBound {
     type Item;
     fn lower_bound(&self, x: &Self::Item) -> Result<usize, usize>;
@@ -18,7 +20,9 @@ pub trait LowerBound {
         K: Ord;
 }
 
-/// find first index where arr[idx] > v; assume arr is sorted
+/// find first index where arr[idx] > v; assume arr is sorted.
+/// it is a encapsulation of `position` method, like `position(|e| e.cmp(x) == Ordering::Greater)`,
+/// but it returns `Err` when all elements are less than or equal to `x`.
 pub trait UpperBound {
     type Item;
     fn upper_bound(&self, x: &Self::Item) -> Result<usize, usize>;
@@ -43,6 +47,8 @@ impl<T: Ord> LowerBound for [T] {
     where
         F: FnMut(&'a Self::Item) -> Ordering,
     {
+        // self.iter().position(|e| f(e) != Ordering::Less).ok_or(self.len())
+
         let mut left = 0;
         let len = self.len();
         let mut right = len;
@@ -80,6 +86,8 @@ impl<T: Ord> UpperBound for [T] {
     where
         F: FnMut(&'a Self::Item) -> Ordering,
     {
+        // self.iter().position(|e| f(e) == Ordering::Greater).ok_or(self.len())
+
         let mut left = 0;
         let len = self.len();
         let mut right = len;
@@ -151,6 +159,20 @@ impl<T: Ord> UpperBound for Vec<T> {
     }
 }
 
+/// To use `lower_bound` and `upper_bound` on `VecDeque`,
+/// you need to call `VecDeque::make_contiguous` first and sort it.
+/// ```
+/// # use bound_stl::{LowerBound, UpperBound};
+/// # use std::collections::VecDeque;
+/// let mut v = VecDeque::new();
+/// v.push_back(1);
+/// v.push_back(2);
+/// v.push_back(3);
+/// assert_eq!(v.lower_bound(&2), Ok(1));
+/// assert_eq!(v.upper_bound(&2), Ok(2));
+/// assert_eq!(v.upper_bound(&0), Ok(0));
+/// assert_eq!(v.lower_bound(&4), Err(3));
+/// ```
 impl<T: Ord> LowerBound for VecDeque<T> {
     type Item = T;
     fn lower_bound(&self, x: &Self::Item) -> Result<usize, usize> {
@@ -198,16 +220,14 @@ impl<T: Ord> UpperBound for VecDeque<T> {
 impl<T: Ord> LowerBound for BinaryHeap<T> {
     type Item = T;
     fn lower_bound(&self, x: &Self::Item) -> Result<usize, usize> {
-        self.iter().position(|y| y >= x).ok_or(self.len())
+        self.lower_bound_by(|y| y.cmp(x))
     }
 
     fn lower_bound_by<'a, F>(&'a self, mut f: F) -> Result<usize, usize>
     where
         F: FnMut(&'a Self::Item) -> Ordering,
     {
-        self.iter()
-            .position(|y| f(y) != Ordering::Less)
-            .ok_or(self.len())
+        self.iter().position(|y| f(y) != Ordering::Less).ok_or(self.len())
     }
 
     fn lower_bound_by_key<'a, K, F>(&'a self, k: &K, mut f: F) -> Result<usize, usize>
@@ -215,16 +235,58 @@ impl<T: Ord> LowerBound for BinaryHeap<T> {
         F: FnMut(&'a Self::Item) -> K,
         K: Ord,
     {
-        self.iter()
-            .position(|y| f(y).cmp(k) != Ordering::Less)
-            .ok_or(self.len())
+        self.lower_bound_by(|y| f(y).cmp(k))
     }
 }
 
 impl<T: Ord> UpperBound for BinaryHeap<T> {
     type Item = T;
     fn upper_bound(&self, x: &Self::Item) -> Result<usize, usize> {
-        self.iter().position(|y| y > x).ok_or(self.len())
+        self.upper_bound_by(|y| y.cmp(x))
+    }
+
+    fn upper_bound_by<'a, F>(&'a self, mut f: F) -> Result<usize, usize>
+    where
+        F: FnMut(&'a Self::Item) -> Ordering,
+    {
+        self.iter().position(|y| f(y) == Ordering::Greater).ok_or(self.len())
+    }
+
+    fn upper_bound_by_key<'a, K, F>(&'a self, k: &K, mut f: F) -> Result<usize, usize>
+    where
+        F: FnMut(&'a Self::Item) -> K,
+        K: Ord,
+    {
+        self.upper_bound_by(|y| f(y).cmp(k))
+    }
+}
+
+impl<T: Ord, V> LowerBound for BTreeMap<T, V> {
+    type Item = T;
+    fn lower_bound(&self, x: &Self::Item) -> Result<usize, usize> {
+        self.lower_bound_by(|y| y.cmp(x))
+    }
+
+    fn lower_bound_by<'a, F>(&'a self, mut f: F) -> Result<usize, usize>
+    where
+        F: FnMut(&'a Self::Item) -> Ordering,
+    {
+        self.iter().position(|(y, _)| f(y) != Ordering::Less).ok_or(self.len())
+    }
+
+    fn lower_bound_by_key<'a, K, F>(&'a self, k: &K, mut f: F) -> Result<usize, usize>
+    where
+        F: FnMut(&'a Self::Item) -> K,
+        K: Ord,
+    {
+        self.lower_bound_by(|y| f(y).cmp(k))
+    }
+}
+
+impl<T: Ord, V> UpperBound for BTreeMap<T, V> {
+    type Item = T;
+    fn upper_bound(&self, x: &Self::Item) -> Result<usize, usize> {
+        self.upper_bound_by(|y| y.cmp(x))
     }
 
     fn upper_bound_by<'a, F>(&'a self, mut f: F) -> Result<usize, usize>
@@ -232,7 +294,7 @@ impl<T: Ord> UpperBound for BinaryHeap<T> {
         F: FnMut(&'a Self::Item) -> Ordering,
     {
         self.iter()
-            .position(|y| f(y) == Ordering::Greater)
+            .position(|(y, _)| f(y) == Ordering::Greater)
             .ok_or(self.len())
     }
 
@@ -241,9 +303,51 @@ impl<T: Ord> UpperBound for BinaryHeap<T> {
         F: FnMut(&'a Self::Item) -> K,
         K: Ord,
     {
-        self.iter()
-            .position(|y| f(y).cmp(k) == Ordering::Greater)
-            .ok_or(self.len())
+        self.upper_bound_by(|y| f(y).cmp(k))
+    }
+}
+
+impl<T: Ord> LowerBound for BTreeSet<T> {
+    type Item = T;
+    fn lower_bound(&self, x: &Self::Item) -> Result<usize, usize> {
+        self.lower_bound_by(|y| y.cmp(x))
+    }
+
+    fn lower_bound_by<'a, F>(&'a self, mut f: F) -> Result<usize, usize>
+    where
+        F: FnMut(&'a Self::Item) -> Ordering,
+    {
+        self.iter().position(|y| f(y) != Ordering::Less).ok_or(self.len())
+    }
+
+    fn lower_bound_by_key<'a, K, F>(&'a self, k: &K, mut f: F) -> Result<usize, usize>
+    where
+        F: FnMut(&'a Self::Item) -> K,
+        K: Ord,
+    {
+        self.lower_bound_by(|y| f(y).cmp(k))
+    }
+}
+
+impl<T: Ord> UpperBound for BTreeSet<T> {
+    type Item = T;
+    fn upper_bound(&self, x: &Self::Item) -> Result<usize, usize> {
+        self.upper_bound_by(|y| y.cmp(x))
+    }
+
+    fn upper_bound_by<'a, F>(&'a self, mut f: F) -> Result<usize, usize>
+    where
+        F: FnMut(&'a Self::Item) -> Ordering,
+    {
+        self.iter().position(|y| f(y) == Ordering::Greater).ok_or(self.len())
+    }
+
+    fn upper_bound_by_key<'a, K, F>(&'a self, k: &K, mut f: F) -> Result<usize, usize>
+    where
+        F: FnMut(&'a Self::Item) -> K,
+        K: Ord,
+    {
+        self.upper_bound_by(|y| f(y).cmp(k))
     }
 }
 
@@ -288,5 +392,26 @@ mod tests {
         assert_eq!(v.upper_bound(&7), Err(8));
         assert_eq!(v.upper_bound(&8), Err(8));
         assert_eq!(v.upper_bound(&9), Err(8));
+    }
+
+    #[test]
+    fn test_lower_bound_btreeset() {
+        use std::collections::BTreeSet;
+        let v = BTreeSet::<i32>::new();
+        assert_eq!(v.lower_bound(&0), Err(0));
+        assert_eq!(v.lower_bound(&1), Err(0));
+
+        let v = vec![1, 2, 4, 5, 5, 6, 6].into_iter().collect::<BTreeSet<_>>();
+        assert_eq!(format!("{v:?}"), "{1, 2, 4, 5, 6}");
+        assert_eq!(v.lower_bound(&0), Ok(0));
+        assert_eq!(v.lower_bound(&1), Ok(0));
+        assert_eq!(v.lower_bound(&2), Ok(1));
+        assert_eq!(v.lower_bound(&3), Ok(2));
+        assert_eq!(v.lower_bound(&4), Ok(2));
+        assert_eq!(v.lower_bound(&5), Ok(3));
+        assert_eq!(v.lower_bound(&6), Ok(4));
+        assert_eq!(v.lower_bound(&7), Err(5));
+        assert_eq!(v.lower_bound(&8), Err(5));
+        assert_eq!(v.lower_bound(&9), Err(5));
     }
 }
